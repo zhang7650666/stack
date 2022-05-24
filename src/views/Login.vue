deeadcd48422114b81debbab5b3bfe1dbbd89df3
Hadwin <zhanghuawei@shengpay.com>
2022-05-24 13:42:19 +0800

    test

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: Hadwin zhanghuawei@shengpay.com
+ * @Date: 2022-05-24 13:41:02
+ * @LastEditors: Hadwin zhanghuawei@shengpay.com
+ * @LastEditTime: 2022-05-24 13:42:05
+ * @FilePath: \stack\src\views\Login.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
-  <div>Login</div>
+  <div>Login6666</div>
 </template>
 <script lang="ts" setup></script>
 <style></style>
